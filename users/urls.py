3ed0e3d38638a7ecc624284abf04e37d3b737a87
Renatazip <56039733+Renatazip@users.noreply.github.com>
2022-03-19 21:59:42 +0300

    new

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    path('users/', views.user_list),
+    path('users/<int:id>', views.user_detail),
+    path('signup/', views.signup),
+    path('profile/', views.profile),
+
+]
